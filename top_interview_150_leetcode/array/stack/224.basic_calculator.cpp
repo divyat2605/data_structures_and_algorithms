60739a30a35d6b148f8b896636c12eebf2a152de
divi2605 <divya.tripathi.2605@gmail.com>
2026-05-08 21:58:01 +0530

    feat: add solution to problem 224-basic-calculator using stack implementaion

---
+++ top_interview_150_leetcode/array/stack/224.basic_calculator.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int calculate(string s) {
+        stack<int> st;
+        int result = 0;
+        long long number = 0;
+        int sign = 1;
+        for (char c:s){
+            if(isdigit(c)){
+                number = number*10 + (c-'0');
+            }
+            else if(c=='+'){
+                result+=sign*number;
+                number =0;
+                sign=1;
+            }
+            else if (c=='-'){
+                result+=sign*number;
+                number =0;
+                sign = -1;
+            }
+            else if(c=='('){
+                st.push(result);
+                st.push(sign);
+                result = 0;
+                sign = 1;
+            }
+            else if (c==')'){
+                result+=sign*number;
+                number =0;
+                int prevSign = st.top();
+                st.pop();
+                 
+                int prevResult = st.top();
+                st.pop();
+                result = prevResult + prevSign*result;
+            }
+        }
+        result+=sign*number;
+        return result;
+    }
+};
